13f5059b6aa946f475899d074e3a8632bbfa152a
rocio43 <98287489+rocio43@users.noreply.github.com>
2022-02-06 23:58:41 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// sidebar open close js code
+let menuLinks = document.querySelector(".menu-links");
+let menuOpenBtn = document.querySelector(".menu-dropdown .bx-menu");
+let menuCloseBtn = document.querySelector(".menu-links .bx-x");
+menuOpenBtn.onclick = function() {
+menuLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+menuLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let arrow1 = document.querySelector(".arrow1");
+arrow1.onclick = function() {
+ menuLinks.classList.toggle("show1");
+}
+let arrow2 = document.querySelector(".arrow2");
+arrow2.onclick = function() {
+ menuLinks.classList.toggle("show3");
+}
