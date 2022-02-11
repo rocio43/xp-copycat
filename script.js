a522d89a180748c2a6fccc6c39d16a0eced99234
rocio43 <98287489+rocio43@users.noreply.github.com>
2022-02-11 00:00:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // sidebar open close js code
-let menuLinks = document.querySelector(".menu-links");
 let menuOpenBtn = document.querySelector(".menu-dropdown .bx-menu");
 let menuCloseBtn = document.querySelector(".menu-links .bx-x");
+let menuLinks = document.querySelector(".menu-links");
 menuOpenBtn.onclick = function() {
-menuLinks.style.left = "0";
+    menuLinks.style.left = "0";
 }
 menuCloseBtn.onclick = function() {
-menuLinks.style.left = "-100%";
+    menuLinks.style.left = "-100%";
 }
 
 
 // sidebar submenu open close js code
 let arrow1 = document.querySelector(".arrow1");
 arrow1.onclick = function() {
- menuLinks.classList.toggle("show1");
+    menuLinks.classList.toggle("show1");
 }
 let arrow2 = document.querySelector(".arrow2");
 arrow2.onclick = function() {
- menuLinks.classList.toggle("show3");
+    menuLinks.classList.toggle("show3");
 }
